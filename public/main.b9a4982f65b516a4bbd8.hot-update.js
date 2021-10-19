c717b60fcdbf803a17ff2db524d3a2f7b455577f
Lucas Guilherme <lguilherme8@gmail.com>
2021-10-18 21:20:32 -0300

    added the navigation. No animations yet.

---
+++ public/main.b9a4982f65b516a4bbd8.hot-update.js
@@ -0,0 +1,22 @@
+self["webpackHotUpdatefloema"]("main",{
+
+/***/ "./styles/index.scss":
+/*!***************************!*\
+  !*** ./styles/index.scss ***!
+  \***************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: expected \";\".\n  ╷\n7 │ @import './pages/home.scss'\n  │                            ^\n  ╵\n  styles/index.scss 7:28  root stylesheet\n    at processResult (/home/lucassilva/backup/projetos/endurance/node_modules/webpack/lib/NormalModule.js:721:19)\n    at /home/lucassilva/backup/projetos/endurance/node_modules/webpack/lib/NormalModule.js:827:5\n    at /home/lucassilva/backup/projetos/endurance/node_modules/loader-runner/lib/LoaderRunner.js:399:11\n    at /home/lucassilva/backup/projetos/endurance/node_modules/loader-runner/lib/LoaderRunner.js:251:18\n    at context.callback (/home/lucassilva/backup/projetos/endurance/node_modules/loader-runner/lib/LoaderRunner.js:124:13)\n    at /home/lucassilva/backup/projetos/endurance/node_modules/sass-loader/dist/index.js:54:7\n    at Function.call$2 (/home/lucassilva/backup/projetos/endurance/node_modules/sass/sass.dart.js:95689:16)\n    at _render_closure1.call$2 (/home/lucassilva/backup/projetos/endurance/node_modules/sass/sass.dart.js:83814:12)\n    at _RootZone.runBinary$3$3 (/home/lucassilva/backup/projetos/endurance/node_modules/sass/sass.dart.js:28060:18)\n    at _FutureListener.handleError$1 (/home/lucassilva/backup/projetos/endurance/node_modules/sass/sass.dart.js:26590:21)");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("222016bbfbaa17fdd5bc")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5iOWE0OTgyZjY1YjUxNmE0YmJkOC5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7VUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL2Zsb2VtYS93ZWJwYWNrL3J1bnRpbWUvZ2V0RnVsbEhhc2giXSwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gKCkgPT4gKFwiMjIyMDE2YmJmYmFhMTdmZGQ1YmNcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=

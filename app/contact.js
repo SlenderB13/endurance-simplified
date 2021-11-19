672063804a05048d1d74170c6d9cf6c7e4ebaa56
Lucas Guilherme <lguilherme8@gmail.com>
2021-11-18 23:07:18 -0300

    inicio do projeto, com páginas home e contact

---
+++ app/contact.js
@@ -0,0 +1,27 @@
+const contactWrapper = document.querySelector('.contact__wrapper')
+
+console.log(contactWrapper.style.top)
+
+const scroll = {
+  current: 0,
+  target: 0,
+  limit: 600
+}
+
+let speed = 0
+
+function update() {
+  window.requestAnimationFrame(update)
+
+  scroll.current = gsap.utils.interpolate(scroll.current, scroll.target, 0.1)
+  scroll.current = gsap.utils.clamp(0, scroll.limit, scroll.current)
+  contactWrapper.style.top = `-${scroll.current}px`
+}
+
+window.addEventListener('mousewheel', (event) => {
+  speed += event.deltaY
+  scroll.target = speed * 0.5
+})
+
+
+update()

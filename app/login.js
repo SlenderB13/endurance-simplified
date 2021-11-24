4787d8914d6c1fb8d5e3cd57ef8a94d321dfb804
Lucas Guilherme <lguilherme8@gmail.com>
2021-11-24 00:16:19 -0300

    adicionada página de login

--- app/login.js
+++ app/login.js
@@ -1,8 +1,7 @@
-const contactWrapper = document.querySelector('.contact__wrapper')
+const loginWrapper = document.querySelector('.login__wrapper')
 const cursor = document.querySelector('.cursor')
-const button = document.querySelector('button')
-
-console.log(contactWrapper.style.top)
+const stripe = document.querySelector('.stripe')
+const buttons = document.querySelectorAll('button')
 
 const scroll = {
   current: 0,
@@ -12,22 +11,24 @@ const scroll = {
 
 let speed = 0
 
-button.addEventListener('mouseover', () => {
-  gsap.to(cursor, {scale: 6, duration: 0.3})
-  button.style.cursor = 'pointer'
+buttons.forEach(button => {
+  button.addEventListener('mouseover', ()=> {
+    gsap.to(cursor, {scale: 6, duration: 0.3})
+    button.style.cursor = 'pointer'
+  })
+  button.addEventListener('mouseleave', ()=> {
+    gsap.to(cursor, {scale: 1, duration: 0.3})
+    button.style.cursor = 'pointer'
+  })
 })
-
-button.addEventListener('mouseleave', () => {
-  gsap.to(cursor, {scale: 1, duration: 0.3})
-})
-
 
 function update() {
   window.requestAnimationFrame(update)
 
   scroll.current = gsap.utils.interpolate(scroll.current, scroll.target, 0.1)
   scroll.current = gsap.utils.clamp(0, scroll.limit, scroll.current)
-  contactWrapper.style.top = `-${scroll.current}px`
+  loginWrapper.style.top = `-${scroll.current}px`
+  stripe.style.top = `-${scroll.current * 0.4}px`
 }
 
 window.addEventListener('mousewheel', (event) => {
